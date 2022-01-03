251edcfeb2ca26383defccbf03529c72bae3004b
Jørgen Riseth <jnriseth@gmail.com>
2022-01-03 10:34:50 +0100

    further archivation

--- experiment/reparamproblem.py
+++ experiment/reparamproblem.py
@@ -3,4 +3,4 @@ from argparse import ArgumentParser
 class ReparametrizationParser(ArgumentParser):
     def __init__(self):
         super().__init__(description="")
-        self.parse_arg(
+        self.parse_arg()
